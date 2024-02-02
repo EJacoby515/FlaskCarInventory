a21ee5cabc8ad0d6e01bac434f601b6e34c31ff5
EJacoby515 <ejacobowitz515@gmail.com>
2024-02-01 23:46:48 -0500

    Added funcionality and DB population

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ class Config():
     FLASK_APP = os.getenv('FLASK_APP')
     FLASK_ENV = os.getenv('FLASK_ENV')
     SECRET_KEY  = os.environ.get('SECRET_KEY') or 'Tresca will always bus the tables'
-    SQLALCHEMY_DATABADE_URI = os.environ.get('DATABASE_URI') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_NOTIFICATIONS = False
